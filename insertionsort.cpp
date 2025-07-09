21b6c16baeed599db3b3c8a164f27df0f0f830f9
sulthann1 <sulthanaawaliya155@gmail.com>
2025-07-09 16:28:09 +0700

    membuat void insertion sort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -20,5 +20,15 @@ void input()
         }
     }
     cout << endl;
-    cout <<
+    cout << "=====================" <<endl;
+    cout << "Masukan Element Array" <<endl;
+    cout << "=====================" <<endl;
+
+    for (int i = 0; i <n; i++)
+    {
+        cout << "Data ke-" << (i+1) <<":";
+        cin >> arr [i];
+    }
 }
+
+void insertionsort ()
